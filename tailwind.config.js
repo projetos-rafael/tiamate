4c08e4d2c7cc2f8692beafcbdf17cffbcc048c90
Rafael Xavier <rafaelnxavier@hotmail.com>
2024-11-14 11:41:39 -0300

    inicio do projeto

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [],
+    theme: {
+      extend: {
+        colors: {
+            'terraCota' : '#64221D',
+            'terraCota' : '#64221D',
+        }
+      },
+    },
+    plugins: [],
+  }
